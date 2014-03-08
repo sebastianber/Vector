4353f0809699b483a491f258d1c5ec1b09600469
10 <10@I10>
2014-03-08 07:42:35 -0500

    new commit

---
+++ src/vector/Vector.java
@@ -0,0 +1,21 @@
+package Vector;
+
+import javax.swing.*;
+public class Vector {
+   public static void main(String[] args) {
+        int num=0;
+         int vec1[]= new int[5];
+         for (int i=0; i<5; i++){
+           num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor de la posicion " +i));
+             if(num>8){
+             vec1[i]= num;
+             }else{
+                 System.out.println("porfavor ingresar numeros mayores  8");
+                  }
+        }
+         for (int i=0; i<5; i++){
+             System.out.println(vec1[i]);
+         }
+         System.out.println();
+   }
+ }
